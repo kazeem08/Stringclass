2b46e82ebbd29196731aef93174ff6b3c917a4b5
Kazeem Jimoh <kazeem0825@gmail.com>
2018-12-19 16:35:43 +0100

    Empty functions added, exported and required in test file

---
+++ functionsAndTests.js/stringClassSolutions.js
@@ -0,0 +1,41 @@
+String.prototype.hasVowels = function () {
+    //Code here
+}
+
+String.prototype.toupper = function () {
+    //Code here
+}
+
+String.prototype.toLower = function () {
+    //Code here
+}
+
+String.prototype.ucFirst = function () {
+    //Code here
+}
+
+String.prototype.isQuestion = function () {
+    //Code here
+}
+
+String.prototype.words = function () {
+    //Code here
+}
+
+String.prototype.wordCount = function () {
+    //Code here
+}
+
+String.prototype.inverseCase = function () {
+    //Code here
+}
+
+String.prototype.alternatingCase = function () {
+    //Code here
+}
+
+String.prototype.numberWords = function () {
+    //Code here
+}
+
+module.exports = String;
