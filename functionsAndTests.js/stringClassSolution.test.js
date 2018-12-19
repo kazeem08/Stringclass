263fbd8cdc0a8a1f35740f04f789dd8ee9bf7c0c
Kazeem Jimoh <kazeem0825@gmail.com>
2018-12-19 16:57:01 +0100

    test cases added for hasVowel function

--- functionsAndTests.js/stringClassSolution.test.js
+++ functionsAndTests.js/stringClassSolution.test.js
@@ -1,1 +1,21 @@
 require("./stringClassSolutions")
+
+describe('test for Vowels', () => {
+    test("string that contain vowels", () => {
+        expect('kaze-m'.hasVowels()).toBeTruthy()
+    })
+    
+    test("string that contain no vowels", () => {
+        expect('jdtddg'.hasVowels()).toBeFalsy()
+    })
+
+    test("string that contain only numbers", () => {
+        expect('123456'.hasVowels()).toBeFalsy()
+    })
+
+    test("string that contain only special characters", () => {
+        expect('jdtddg'.hasVowels()).toBeFalsy()
+    })
+    
+
+})
